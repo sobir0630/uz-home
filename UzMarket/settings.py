ea7fdb7fc35a13950ef7aec6f78f2cd9da0f3854
sobir0630 <mamasoliyevs538@gmail.com>
2026-01-12 11:39:55 +0500

    staticlar render uchun tugrilandi

--- UzMarket/settings.py
+++ UzMarket/settings.py
@@ -129,10 +129,14 @@ AUTH_USER_MODEL = 'users.User'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # static files
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-] 
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # backends
 AUTHENTICATION_BACKENDS = [
